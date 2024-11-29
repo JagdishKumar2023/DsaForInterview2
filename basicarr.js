6fd14365f4cfaa9a8bcdb9e42cd112fea276d3eb
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-11-29 16:54:10 +0530

    New changes

--- basicarr.js
+++ basicarr.js
@@ -33,3 +33,34 @@ for (let i = 0; i < arr.length; i++) {
 // while (i < arr.length) {
 //   i++;
 // }
+
+//remove dulicate from array
+
+function removeDulicate(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(removeDulicate([1, 2, 2, 3, 4, 4, 5, 5, 6, 6, 7, 5, 7]));
+
+// Other methods
+
+let uniqueSet = new Set([1, 2, 5, 5, 6, 8, 9, 9, 4, 3, 6, 1, 1, 153, 2135]);
+console.log(uniqueSet, "Set it");
+
+// insert in array without any method
+
+let arrInsert = [1, 3, 6];
+let ele = 9;
+
+arrInsert[arrInsert.length] = ele;
+
+console.log(arrInsert);
+
+// sorting in array
+
+let arrSort = [3, 4, 5, 8, 9, 7, 6, 1, 2];
+
+arrSort.sort((f, g) => f - g); // ascending order
+arrSort.sort((f, g) => g - f); // decending order
+
+console.log(arrSort);
